8f85ea6918fb80ad4d1f96ceefcd920dd9f6c2b5
Emmanuel Rojas <mamueljr@gmail.com>
2025-11-17 21:21:03 -0600

    Update vite.config.ts

--- v2/vite.config.ts
+++ v2/vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   // Ajusta `base` solo si despliegas en un subdirectorio. Para desarrollo local usa '/'.
-  base: '/',
+  base: '/met_avanzada/v2/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
